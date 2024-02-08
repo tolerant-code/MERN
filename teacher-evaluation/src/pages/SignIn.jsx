76cc6a6276aa82b6768c6d917f1621cfe4a249ae
tolerant-code <tolerantfull@gmail.com>
2024-02-08 14:40:33 +0300

    add redux toolkit

--- teacher-evaluation/src/pages/SignIn.jsx
+++ teacher-evaluation/src/pages/SignIn.jsx
@@ -1,10 +1,12 @@
 import { Alert, Button, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector} from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'; 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim()});
@@ -12,11 +14,10 @@ export default function SignIn() {
   const handleSubmit = async(e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields");
+      return dispatch(signInFailure('Please fill all required fields'));
     }
     try {
-      setLoading(true)
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -24,17 +25,16 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } 
     catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
-    }
+      dispatch(signInFailure(error.message));
+          }
   };
   return (<div className='min-h-screen mt-20'>
     <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
